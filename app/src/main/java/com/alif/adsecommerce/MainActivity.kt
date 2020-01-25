38014bd2319829a16588fdd79101d9fa3286b291
Adhamsheriff A <adhamsheriff.a@vajro.com>
2020-01-25 16:47:10 +0530

    * Added DrawerLayout with NavigationView with Icon

--- app/src/main/java/com/alif/adsecommerce/MainActivity.kt
+++ app/src/main/java/com/alif/adsecommerce/MainActivity.kt
@@ -1,16 +1,11 @@
 package com.alif.adsecommerce
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.alif.adsecommerce.model.Product
-
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.android.synthetic.main.main.*
 
 class MainActivity : AppCompatActivity() {
 
