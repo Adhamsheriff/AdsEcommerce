99c3416702646a7ccadf2d7f91f2ada5ff2df40c
Adhamsheriff A <adhamsheriff.a@vajro.com>
2020-01-25 17:04:23 +0530

    * Opening DrawerLayout with Icon

--- app/src/main/java/com/alif/adsecommerce/MainActivity.kt
+++ app/src/main/java/com/alif/adsecommerce/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.alif.adsecommerce
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import com.alif.adsecommerce.model.Product
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import kotlinx.android.synthetic.main.main.*
 
@@ -14,21 +17,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        navigationView.setNavigationItemSelectedListener {
+            it.isChecked = true
+            drawerLayout.closeDrawers()
+            true
+        }
+
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp)
+        }
+
         val products = arrayListOf<Product>()
-        for(i in 0..100){
+        for (i in 0..100) {
             products.add(
                 Product(
-            "Shirt #$i",
-                "https://via.placeholder.com/300.png/dddddd/000000",
-                1.99
-            )
+                    "Shirt #$i",
+                    "https://via.placeholder.com/300.png/dddddd/000000",
+                    1.99
+                )
             )
 
         }
 
         recycler_view.apply {
-            layoutManager = GridLayoutManager(this@MainActivity,2)
+            layoutManager = GridLayoutManager(this@MainActivity, 2)
             adapter = ProductAdapter(products)
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        drawerLayout.openDrawer(GravityCompat.START)
+        return true
+//        return super.onOptionsItemSelected(item)
     }
 }
