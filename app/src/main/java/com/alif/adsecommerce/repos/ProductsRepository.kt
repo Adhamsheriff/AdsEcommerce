2e2318199d7d2ba095d2911173204ffdfac1cd7c
Adhamsheriff A <adhamsheriff.a@vajro.com>
2020-01-27 02:21:48 +0530

    * Change URL from github.

--- app/src/main/java/com/alif/adsecommerce/repos/ProductsRepository.kt
+++ app/src/main/java/com/alif/adsecommerce/repos/ProductsRepository.kt
@@ -9,7 +9,7 @@ class ProductsRepository {
 
     fun getAllProducts(): Single<List<Product>> {
         return Single.create<List<Product>> {
-            val json = URL("https://finepointmobile.com/data/products.json").readText()
+            val json = URL("https://gist.githubusercontent.com/Adhamsheriff/f4fdd5bbebee5d4cbba8c5e84bfffb33/raw/1fea1f05c0bff979ce21604e993e1ba21a2c9162/products_list.json").readText()
             val products = Gson().fromJson(json, Array<Product>::class.java).toList()
             it.onSuccess(products)
         }
